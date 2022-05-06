1eafd521491ebcfacba9be5297fc85c514f1a77e
38810757 <99075650+38810757@users.noreply.github.com>
2022-05-06 17:17:22 +0300

    change1

---
+++ stan.js
@@ -0,0 +1,48 @@
+const card = document.querySelector(".card");
+const contacts = document.querySelector(".contacts");
+const drawn1 = document.querySelector(".drawn1");
+const carens = document.querySelector(".carens");
+const carens1 = document.querySelector(".carens1");
+const carens2 = document.querySelector(".carens2");
+const carens3 = document.querySelector(".carens3");
+const carens4 = document.querySelector(".carens4");
+const carens5 = document.querySelector(".carens5");
+const carton = document.querySelector(".cartoon");
+const carton1 = document.querySelector(".cartoon1");
+const carton2 = document.querySelector(".cartoon2");
+const carton3 = document.querySelector(".cartoon3");
+const carton4 = document.querySelector(".cartoon4");
+const carton5 = document.querySelector(".cartoon5");
+const carton6 = document.querySelector(".cartoon6");
+const carton7 = document.querySelector(".cartoon7");
+const carton8 = document.querySelector(".cartoon8");
+const carton9 = document.querySelector(".cartoon9");
+const carton10 = document.querySelector(".cartoon10");
+const carton11 = document.querySelector(".cartoon11");
+
+
+
+
+const tl = new TimelineMax();
+
+tl.fromTo(card,1.5, {height: "0%"}, {height: '400px' , ease: Power2.easeInOut });
+tl.fromTo(contacts,1, {height: "0%"}, {height: '400px'});
+tl.fromTo(drawn1,1, {height: "0%"}, {height: '250px'});
+tl.fromTo(carens,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(carens1,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(carens2,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(carens3,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(carens4,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(carens5,1, {height: "0%"}, {height: '230px'});
+tl.fromTo(cartoon,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon1,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon2,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon3,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon4,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon5,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon6,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon7,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon8,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon9,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon10,1, {height: "0%"}, {height: '600px'});
+tl.fromTo(cartoon11,1, {height: "0%"}, {height: '600px'});
